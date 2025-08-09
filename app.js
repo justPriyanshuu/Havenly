2e30e3e527b3b98ce4a30c0f02b9f5c94ede05dd
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-08-09 22:22:21 +0530

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('../Havenly/models/listing.js');
+const path = require('path');
 
 main().catch((err) => console.log(err));
 
@@ -9,21 +10,29 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/Havenly');
 }
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
   res.send('Hi , i am root');
 });
 
-app.get('/testlisting', async (req, res) => {
-  const samplelisting = new Listing({
-    title: 'new villa',
-    description: 'Near the beach',
-    price: 1500,
-    location: 'goa',
-    country: 'india',
-  });
-  await samplelisting.save();
-  res.send('testing complete');
+app.get('/listing', async (req, res) => {
+  const allLisitngs = await Listing.find({});
+  res.render('home.ejs', { allLisitngs });
 });
+
+// app.get('/testlisting', async (req, res) => {
+//   const samplelisting = new Listing({
+//     title: 'new villa',
+//     description: 'Near the beach',
+//     price: 1500,
+//     location: 'goa',
+//     country: 'india',
+//   });
+//   await samplelisting.save();
+//   res.send('testing complete');
+// });
 
 app.listen(8080, () => {
   console.log('server is listening to port 8080');
