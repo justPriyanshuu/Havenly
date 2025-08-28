f2bd5b2c9f7b6cf931d92157fab7bca39b4405b5
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-08-28 12:56:43 +0530

    update

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const sessionOptions = {
   secret: 'mysecretCode',
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.use(session(sessionOptions));
