670213b7407fe5ae260f18ff73d3f0229f60f279
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-08-29 10:16:43 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/signup', (req, res) => {
   res.render('users/signup.ejs');
@@ -23,5 +24,14 @@ router.post('/signup', async (req, res) => {
 router.get('/login', (req, res) => {
   res.render('users/login.ejs');
 });
+
+router.post(
+  '/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: 'true' }),
+  async (req, res) => {
+    req.flash('Welcome back to Havenly!');
+    res.redirect('/listings');
+  }
+);
 
 module.exports = router;
