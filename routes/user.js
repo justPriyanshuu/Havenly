5ff8d1d0e3fb0c6cfe274b53557777c60f439a7c
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-08-29 09:51:58 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/signup', (req, res) => {
   res.render('users/signup.ejs');
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.flash('success', 'Welcome to Havenly');
+    res.redirect('/listings');
+  } catch (e) {
+    req.flash('error', 'Some error occured');
+    res.redirect('/signup');
+  }
+});
+
+router.get('/login', (req, res) => {
+  res.render('users/login.ejs');
 });
 
 module.exports = router;
