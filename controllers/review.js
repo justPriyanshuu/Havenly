c7285c7d79b4b5a827e140e7393adb2d9e9b3e6b
Priyanshu-xdev <pmaurya.dev@gmail.com>
2025-08-30 10:36:24 +0530

    update

--- controllers/review.js
+++ controllers/review.js
@@ -1,1 +1,33 @@
-module.exoprts
+const Listing = require('../models/listing');
+const Review = require('../models/review');
+
+module.exports.createReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      return res.status(404).send('Listing not found');
+    }
+    const newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    await newReview.save();
+    listing.reviews.push(newReview);
+    await listing.save();
+    req.flash('success', 'New Review Created!');
+    res.redirect(`/listings/${listing._id}`);
+  } catch (e) {
+    res.status(500).send('Something went wrong');
+  }
+};
+
+module.exports.destroyReview = async (req, res) => {
+  try {
+    const { id, reviewId } = req.params;
+    await Review.findByIdAndDelete(reviewId);
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    req.flash('error', 'Review Deleted!');
+    res.redirect(`/listings/${id}`);
+  } catch (e) {
+    res.status(500).send('Something went wrong');
+  }
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const Listing = require('../models/listing');
-const Review = require('../models/review');
+
 const { isLoggedIn, isAuthor } = require('../middleware');
+const reviewController = require('../controllers/review')
 
 //Review Post
-router.post('/', isLoggedIn, );
+router.post('/', isLoggedIn, reviewController.createReview);
 
 //Review Delete
-router.delete('/:reviewId', isLoggedIn, isAuthor, async (req, res) => {
-  try {
-    const { id, reviewId } = req.params;
-    await Review.findByIdAndDelete(reviewId);
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    req.flash('error', 'Review Deleted!');
-    res.redirect(`/listings/${id}`);
-  } catch (e) {
-    res.status(500).send('Something went wrong');
-  }
-});
+router.delete('/:reviewId', isLoggedIn, isAuthor, reviewController.destroyReview);
 
 module.exports = router;
